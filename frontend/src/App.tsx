a9b6bbcf36252689cd6d31a4109b86660e3cab8c
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-10-13 16:02:35 -0500

    clerk auth setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+// example of how clerk login can be used. Clerk made login components automatically
+
 import HomePage from './pages/HomePage/Home';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
-  return <HomePage />;
+  return (
+    <header>
+      
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <HomePage/>
+      
+    </header>
+  );
 }
 
 export default App;
