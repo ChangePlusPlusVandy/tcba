52d326cefc639857670b8e738fb9bdc10a578ad0
JoshJ7792 <joshua.y.jung@vanderbilt.edu>
2025-10-19 14:04:03 -0500

    added survey basic model + functions

--- backend/config/clerk.ts
+++ backend/config/clerk.ts
@@ -1,4 +1,6 @@
 import { createClerkClient } from '@clerk/express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 if (!process.env.CLERK_SECRET_KEY) {
   throw new Error('CLERK_SECRET_KEY environment variable is required');
---
+++ backend/routes/surveyRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import {
+  getAllSurveys,
+  getSurveyById,
+  deleteSurvey,
+} from '../controllers/surveyController.js';
+
+
+const router = Router();
+
+router.get('/', getAllSurveys);
+router.get('/:id', getSurveyById);
+router.delete('/:id', deleteSurvey);
+
+export default router;
