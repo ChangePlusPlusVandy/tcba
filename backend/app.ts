f75ec793867af72eaec812fa5e1b4fbfbbf13b6d
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-10-23 19:34:52 -0500

    general fixes

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import organizationRoutes from './routes/organizationRoutes';
 import announcementRoutes from './routes/announcementRoutes';
 import emailSubscriptionRoutes from './routes/emailSubscriptionRoutes';
+import adminRoutes from './routes/adminRoutes';
 import { prisma } from './config/prisma';
 
 const app = express();
@@ -17,5 +18,6 @@ app.get('/api/health', (req, res) => {
 app.use('/api/organizations', organizationRoutes);
 app.use('/api/announcements', announcementRoutes);
 app.use('/api/subscriptions', emailSubscriptionRoutes);
+app.use('/api/admin', adminRoutes);
 
 export default app;
