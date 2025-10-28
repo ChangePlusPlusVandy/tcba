9c199a724eddb5fdfe2a4b1cb8f42889231bef84
LyTrv-20 <lyvtran59563@gmail.com>
2025-10-28 16:18:04 -0500

    basic clerk backend + format

--- backend/app.ts
+++ backend/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import organizationRoutes from './routes/organizationRoutes';
 import { prisma } from './config/prisma';
+import { clerkClient, clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Server is running!' });
--- backend/controllers/subEmailController.ts
+++ backend/controllers/subEmailController.ts
@@ -1,1 +1,12 @@
-export const getEmailById 
+import { OrganizationRole } from '.prisma/client';
+import { prisma } from '../config/prisma.js';
+
+//keep track the emails that would send out
+const isAdmin = (role?: OrganizationRole) => role === 'ADMIN' || role === 'SUPER_ADMIN';
+
+export const createSubEmail = async () => {
+  try {
+  } catch (error) {
+    console.error('Error creating sub email:', error);
+  }
+};
