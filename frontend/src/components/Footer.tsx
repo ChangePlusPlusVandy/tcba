5ff8983cc7fcfc04cd9f239e18b88b128574f7e7
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-10-30 23:46:59 -0500

    fixed footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import tcbaFooterLogo from '../assets/tcbaFooter.png';
 
 const Footer = () => {
   return (
-    <footer className='w-full bg-white border-t border-gray-200 mt-auto'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
-        <div className='flex justify-center'>
+    <footer className='w-full mt-auto' style={{ backgroundColor: '#EBF3FF' }}>
+      <div className='w-full px-4 sm:px-6 lg:px-8 py-8'>
+        <div className='flex justify-start'>
           <img src={tcbaFooterLogo} alt='TN Coalition for Better Aging' className='h-16 w-auto' />
         </div>
       </div>
