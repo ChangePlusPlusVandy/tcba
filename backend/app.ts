c8ed64210b9074a6cbfec2b38e9a9e272174cd88
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-11-17 00:02:33 -0600

    cors policy

--- backend/app.ts
+++ backend/app.ts
@@ -23,7 +23,7 @@ import { clerkClient, clerkMiddleware } from '@clerk/express';
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://tcba-frontend.vercel.app',
     credentials: true,
   })
 );
