37bf897d64de4347975a0b4c26a925067814bd3f
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-12-31 17:13:16 -0500

    prod fixes

--- frontend/src/config/quill.ts
+++ frontend/src/config/quill.ts
@@ -1,13 +1,12 @@
-import { Quill } from 'react-quill-new';
-import ImageResize from 'quill-image-resize-module-react';
-
 let registered = false;
 
-export const registerQuillModules = () => {
+export const registerQuillModules = async () => {
   if (registered) return;
 
   try {
     if (typeof window !== 'undefined') {
+      const { Quill } = await import('react-quill-new');
+      const ImageResize = (await import('quill-image-resize-module-react')).default;
       Quill.register('modules/imageResize', ImageResize);
       registered = true;
     }
